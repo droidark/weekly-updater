cafa0e5950f762c238da361fada4e833839df118
droidark <droidark@outlook.com>
2024-08-01 13:23:55 -0600

    Little changes on properties

--- src/main/java/xyz/krakenkat/weeklyupdater/util/Constants.java
+++ src/main/java/xyz/krakenkat/weeklyupdater/util/Constants.java
@@ -2,5 +2,5 @@ package xyz.krakenkat.weeklyupdater.util;
 
 public class Constants {
     private Constants() {}
-    public static final String QUERY = "select key, title, whakoom_url, publisher_url from \"%s\" where key in (%s)";
+    public static final String QUERY = "select key, title_id, whakoom_url, publisher_url from \"%s\" where key in (%s)";
 }
